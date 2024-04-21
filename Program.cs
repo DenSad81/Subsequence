8af6a75381b560641548aef2127e421d79d1c07e
DenSad81 <tagil81@mail.ru>
2024-04-21 06:23:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int startNumber = 5;
+        int stopNumber = 96;
+        int increment = 7;
+        int actualValue;
+
+        for (actualValue = startNumber; actualValue <= stopNumber; actualValue += increment)
+        {
+            Console.WriteLine(actualValue);
+        }
+
+        //т.к. мы знаем точные параметры нашего цикла, а именно - начальное значение, 
+        // конечное значение и инкремент, то исходя из этого выбран цикл for, 
+        // хотя можно использовать и цикл while
+    }
+}
